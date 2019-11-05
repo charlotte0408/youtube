5b9579264ab946120044991e896a2c3fcf4f5e06
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-11-05 00:26:42 -0800

    creating growth dataframe

--- 10:31 trending_time/trending_time.R
+++ 10:31 trending_time/trending_time.R
@@ -9,4 +9,31 @@ data_frame <- mutate(data, trending_month = month(formatted_trending_date),
                      trending_year = year(formatted_trending_date))
 head(sort(table(data_frame$video_id), decreasing = TRUE), n = 50)
 hist(table(data_frame$video_id))
+fivenum(table(data_frame$video_id)) ## top 25% stay on the trending list for more than 8 days
 
+pop_list = levels(data_frame$video_id)[table(data_frame$video_id) > 8]
+df_trend = data_frame[data_frame$video_id %in% pop_list, ]
+dim(df_trend)
+category_count <- df_trend %>% count(category = category_id) %>% data.frame()
+order <- order(category_count$n, decreasing = TRUE)
+category_count$category[order] ## same as the general case, top categories are still top
+
+df_trend$LikeRatio = df_trend$likes / df_trend$views
+
+df_growth = data.frame()
+
+for (id in pop_list){
+  id_df = df_trend[df_trend$video_id == id, ]
+  for (j in seq(2, dim(id_df)[1])){
+    df_growth <- rbind(df_growth, data.frame(id, table(data_frame$video_id)[[id]], 
+                              (id_df[j, 'likes'] - id_df[j-1, 'likes']) / id_df[j-1, 'likes'], 
+                              (id_df[j, 'comment_count'] - id_df[j-1, 'comment_count']) / id_df[j-1, 'comment_count'], 
+                              (id_df[j, 'views'] - id_df[j-1, 'views']) / id_df[j-1, 'views'],
+                              (id_df[j, 'LikeRatio'] - id_df[j-1, 'LikeRatio']) / id_df[j-1, 'LikeRatio']))
+  }
+}
+colnames(df_growth) <- c('video_id', 'trending_days', 'Glikes', 'Gcomments', 'Gviews', 'GLikeRatio')
+
+plot(seq(1:9), df_g$Glikes, type="l")
+lines(seq(1:9),df_g$Gviews, col="red")
+lines(seq(1:9),df_g$Gcomments, col="green")
